23737d864bf6391800056dd210fce02ec14500e8
Leykun-Gizaw <Leykungizaw@gmail.com>
2025-08-07 14:49:24 +0300

    feat(lms): Create a test file and json data

---
+++ main.py
@@ -0,0 +1,33 @@
+import json
+from models.book import Book
+from models.library import Library
+from models.users import Member
+from services.library_service import LibraryService
+
+
+def main():
+    # Create infrastructure
+    library = Library("My local library")
+    library_service = LibraryService(library)
+
+    # Add books
+    books_list = []
+    with open("./data/books.json", "r") as file:
+        books_list = json.load(file)
+
+    for book in books_list:
+        library.add_book(book=Book(book["title"], book["author"], book["isbn"]))
+
+    # Register user
+    user = Member("Leykun")
+    user2 = Member("Bisrat")
+    library.register_user(user)
+
+    try:
+        library_service.checkout_book(user, "Non-existent book")
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    main()
