28ee6a13b55b5fe39289c63f23cb12fe7e0d2c4f
Leykun-Gizaw <Leykungizaw@gmail.com>
2025-08-29 13:49:38 +0300

    refactor(lms): Remove files and folders to setup Django

--- exceptions/custom_exceptions.py
+++
@@ -1,8 +0,0 @@
-class BookNotAvailableError(Exception):
-    def __init__(self, book_title: str) -> None:
-        super().__init__(f"The book '{book_title}' is currently not available")
-
-
-class BookNotFoundError(Exception):
-    def __init__(self, book_title: str) -> None:
-        super().__init__(f"The book '{book_title}' can not be found")
