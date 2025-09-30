4eef525fa6f186cd89467c3a64ef8c27c233dc8e
Leykun-Gizaw <Leykungizaw@gmail.com>
2025-09-30 17:12:20 +0300

    Create core app

--- myproject/settings.py
+++ myproject/settings.py
@@ -35,7 +35,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "library_app.apps.LibraryAppConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
