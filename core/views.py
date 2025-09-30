1b471e34b2896aa42d7b3832d2236e8011269954
Leykun-Gizaw <Leykungizaw@gmail.com>
2025-09-30 18:05:20 +0300

    feat(api): ✨ Implement Book API with serializers, views, and routing

--- core/views.py
+++ core/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets, permissions
+
+from .serializers import BookSerializer
+from .models import Book
+
+
 # Create your views here.
+class BookViewSet(viewsets.ModelViewSet):
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Book.objects.filter(library=user.employee.library)
+
+    def perform_create(self, serializer):
+        serializer.save(library=self.request.user.employee.library)
